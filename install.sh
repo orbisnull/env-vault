1b4aace0e17cdef5abd8808fb52572e2c248acb9
orbisnull <orbisnull@zohomail.eu>
2021-08-23 09:43:51 +0800

    fix requirements.txt and add it use in install script

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ if [[ ! -e $dir ]]; then
     mkdir $dir
 fi
 
+wget -O /opt/env-vault/requirements.txt https://raw.githubusercontent.com/orbisnull/env-vault/main/requirements.txt
+pip3 install -r /opt/env-vault/requirements.txt
+
 wget -O /opt/env-vault/env-load.sh https://raw.githubusercontent.com/orbisnull/env-vault/main/env-load.sh
 chmod +x /opt/env-vault/env-load.sh
 
